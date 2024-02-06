15b979a74c27f229501778fbed22037acc29c517
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-02-06 16:58:21 -0300

    New tests waiting for classes

---
+++ BackFrontCursoYt/src/main/java/com/mycompany/backfrontcursoyt/BackFrontCursoYt.java
@@ -0,0 +1,13 @@
+package com.mycompany.backfrontcursoyt;
+
+/**
+ *
+ * @author danie
+ */
+public class BackFrontCursoYt {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(1+1);
+    }
+}
