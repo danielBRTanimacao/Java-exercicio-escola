53ba170a924515ba61cb75dfa7f15329804da001
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-02-15 18:33:51 -0300

    studying a little Java

--- BackFrontCursoYt/src/main/java/com/mycompany/backfrontcursoyt/BackFrontCursoYt.java
+++ BackFrontCursoYt/src/main/java/com/mycompany/backfrontcursoyt/BackFrontCursoYt.java
@@ -8,9 +8,21 @@ package com.mycompany.backfrontcursoyt;
 // Comitando tipos de commit
 
 public class BackFrontCursoYt {
-
+      
     public static void main(String[] args) {
+        String person = "Daniel";
+        int age = 19;
+        float height = 103.4f;
+        double high = 1.85;
+        char sex = 'M';
+        boolean is_legal_age = age >= 18;
+        
         System.out.println("Hello World!");
-        System.out.println(1+1);
+        System.out.println("Hi, my name is.. " + person);
+        System.out.println("i'm " + age + " years old");
+        System.out.println("my height is " + height);
+        System.out.println("but my high is " + high);
+        System.out.println("my sex is " + sex);
+        System.out.println("Am I finally of age? " + is_legal_age);
     }
 }
