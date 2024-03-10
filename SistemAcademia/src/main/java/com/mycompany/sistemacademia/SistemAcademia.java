15dc5580227ab022f6908e0960ee757537ce4cd2
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-10 15:53:54 -0300

    testes facul

---
+++ SistemAcademia/src/main/java/com/mycompany/sistemacademia/SistemAcademia.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemacademia;
+
+/**
+ *
+ * @author danie
+ */
+public class SistemAcademia {
+
+    public static void main(String[] args) {
+        float kilos = 90.5f;
+        String name = "daniel";
+        System.out.println(kilos + "Kg, " + "nome: " + name);
+    }
+}
