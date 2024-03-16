6927329b7b0064e006c0943f7ed9e31712b10fa1
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-16 18:08:17 -0300

    aulas java

--- portfolioUnopar/gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
+++ portfolioUnopar/gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
@@ -8,9 +8,20 @@ package com.mycompany.gerenciabanco;
  *
  * @author danie
  */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GerenciaBanco {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        dataPerson("daniel", 19, 101, 1.85f, 'M');
+        
     }
+    
+    public static void dataPerson(String person, int age, double weight, float height, char sex) {
+        List<String> dataBasePerson = new ArrayList<>();
+        dataBasePerson.add(person);
+    }
+    
 }
