d9111005fd0a2f3ab2432424a5e12314ac27b3f9
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-23 13:38:15 -0300

    Studing java

---
+++ UnoparEstudo/src/DefinirPeso.java
@@ -0,0 +1,8 @@
+public class DefinirPeso {
+    public static void main(String[] args) throws Exception {
+        String nome = "daniel";
+        int idade = 19;
+        double peso = 98.3;
+        System.out.printf("%s, %d anos, pesa %.2f Kg", nome, idade, peso);
+    }
+}
