04cba0281aafb5783516b9f09840bc7d08234bd4
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-29 11:33:41 -0300

    init project porftolio

--- portfolioUnopar/gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
+++ portfolioUnopar/gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
@@ -9,26 +9,10 @@ package com.mycompany.gerenciabanco;
  * @author danie
  */
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class GerenciaBanco {
 
     public static void main(String[] args) {
-        dataPerson("daniel", "19", "101", "1.85", "M");
+        System.out.println("Ola!");
         
-    }
-    
-    public static void dataPerson(String person, String age, String weight, String height, String sex) {
-        List<String> lista = new ArrayList<>();
-        lista.add(person);
-        lista.add(age);
-        lista.add(weight);
-        lista.add(height);
-        lista.add(sex);
-        
-        for (int i = 0; i < lista.size(); i++) {
-            System.out.println(lista.get(i));
-        }
     }
 }
