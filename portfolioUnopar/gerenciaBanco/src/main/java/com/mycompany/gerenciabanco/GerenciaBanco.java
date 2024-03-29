a0128eb55837ec807e73934cf4ae0cd7ee1448fa
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-29 12:08:53 -0300

    making class client

--- portfolioUnopar/gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
+++ portfolioUnopar/gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
@@ -9,6 +9,21 @@ package com.mycompany.gerenciabanco;
  * @author danie
  */
 
+import java.util.Scanner;
+
+class Client {
+    private String name;
+    private String last_name;
+    private String cpf;
+    
+    private double cash_balance;
+
+    
+    public Client(String name, String last_name, String cpf) {
+        
+    }
+}
+
 public class GerenciaBanco {
 
     public static void main(String[] args) {
