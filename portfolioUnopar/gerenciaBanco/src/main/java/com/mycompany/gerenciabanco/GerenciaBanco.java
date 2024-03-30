ebf2f57d795ccd2d50caeb1768e83e26a51a69e7
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-30 12:28:03 -0300

    creating class

--- portfolioUnopar/gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
+++ portfolioUnopar/gerenciaBanco/src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
@@ -14,24 +14,45 @@ import java.util.Scanner;
 class Client {
     private String name;
     private String last_name;
+    private int age;
     private String cpf;
     
     private double cash_balance;
 
-    public Client(String name, String last_name, String cpf) {
+    public Client(String name, String last_name, int age, String cpf) {
         this.name = name;
         this.last_name = last_name;
+        this.age = age;
         this.cpf = cpf;
         this.cash_balance = 0;
     }
     
-    public static void withdrawMoney() {
+    public static void checkBalance() {
         System.out.println("Teste");
+    }
+    
+    public static void makeDeposit() {
+        System.out.println("Depositando");
+    }
+    
+    public static void withdrawMoney() {
+        System.out.println("Sacando");
+    }
+
+}
+
+class Manager extends Client {
+    
+    public Manager() {
+        
+    }
 }
 
 public class GerenciaBanco {
 
     public static void main(String[] args) {
-        System.out.println("testes de novo")
+        System.out.println("testes de novo");
+        
+        // Client person = new Client(name, last_name, age, cpf);
     }
 }
