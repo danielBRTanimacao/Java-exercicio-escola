6c71c038b73bff5add807f4748b0856e6198b02a
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-20 12:42:36 -0300

    teste spring boot vs code

---
+++ TestSpringBoot/testes/src/test/java/com/danieltenorio/testes/TestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danieltenorio.testes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
