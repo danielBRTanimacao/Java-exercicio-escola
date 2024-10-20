f5baafc303230366e9643074d7bbac143519a50b
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-10-20 12:49:05 -0300

    new tests

--- estudo-java-yt/src/main/java/com/daniel/Game.java
+++ estudo-java-yt/src/main/java/com/daniel/Game.java
@@ -1,26 +1,39 @@
 package com.daniel;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
-        boolean counter = true;
-        while (counter) {
+        int counter = 1;
+        System.out.println("==========================");
+        System.out.println(ConsoleColors.BLACK_BOLD + "Você tem " + ConsoleColors.GREEN + "5" + ConsoleColors.RESET + ConsoleColors.BLACK_BOLD + " tentativas!" + ConsoleColors.RESET);
+        while (counter < 6) {
             System.out.println("==========================");
-            System.out.println(ConsoleColors.BLACK_BOLD +"Digite um valor de 0 a 10:" + ConsoleColors.RESET);
+            System.out.println(ConsoleColors.BLACK_BOLD + "Digite um valor de 0 a 10:" + ConsoleColors.RESET);
             System.out.println("==========================");
 
             int randomValue = getRandomValue();
-            int valueChoiced = userChosedValue();
+            int valueChoiced;
+
+            try {
+                valueChoiced = userChosedValue();
+            } catch (InputMismatchException e) {
+                System.out.println(ConsoleColors.YELLOW + "INVALIDO!" + ConsoleColors.RESET + " Digite um valor valido não pode conter caracteres");
+                continue;
+            }
+
             if (valueChoiced > 10 || valueChoiced < 0) {
                 System.out.println(ConsoleColors.YELLOW + "INVALIDO!" + ConsoleColors.RESET + " o valor não pode ser maior que 10 ou menor que 0 ");
+                counter++;
             }
             else if (randomValue == valueChoiced) {
                 System.out.println(ConsoleColors.GREEN + "ACERTOU!" + ConsoleColors.RESET + " o valor e " + randomValue);
-                counter = false;
+                break;
             } else {
-                System.out.println(ConsoleColors.RED + "ERROU!" + ConsoleColors.RESET + " o valor e " + randomValue);
+                System.out.println(ConsoleColors.RED + "ERROU! " + ConsoleColors.RESET + counter +"/5 tentativas" + " o valor era " + randomValue);
+                counter++;
             }
         }
     }
