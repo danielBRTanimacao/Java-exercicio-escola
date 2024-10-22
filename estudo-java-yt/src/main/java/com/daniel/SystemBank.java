cd2de6e65244b6e6fdc529518d62e352f4556659
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-10-22 12:41:12 -0300

    making new tests on java

--- estudo-java-yt/src/main/java/com/daniel/SystemBank.java
+++ estudo-java-yt/src/main/java/com/daniel/SystemBank.java
@@ -2,27 +2,50 @@ package com.daniel;
 
 public class SystemBank {
     public static void main(String[] args) {
-        Person user = new Person("Bico Seco", 10, "nenhum", 12.3);
-        Manager manager = new Manager("Gerente", 23, "Bank", 100, true);
+        Person user = new Person("Bico Seco", 18, "Clt 7x0", 1350);
+        Manager manager = new Manager("Gerente", 23, "Bank", 3000, true);
         Bank bras = new Bank(user, manager);
 
-        bras.takeData();
+        System.out.println(bras.takeData());
+        bras.deposit(100);
+        bras.withdrawn(1);
+        bras.makeLoan(2000);
     }
 }
 
 class Bank {
-    double amount = 10000;
+    double limitBank = 10000;
+    double limitUser;
+    double amountUser = 0;
     Person userBank;
     Manager managerBank;
 
     public Bank(Person userBank, Manager managerBank){
-        System.out.println("Qual ação deseja fazer?");
         this.userBank = userBank;
+        this.limitUser = userBank.salary / 2; 
         this.managerBank = managerBank;
     }
 
     public String takeData(){
-        return "";
+        return "==========================\nOlá, " + this.userBank.name + " seus dados:\nIdade: " + this.userBank.age +"\nTrabalho: " + this.userBank.work +"\nDinheiro: " + this.userBank.salary + "\nLimite: " + this.limitUser + "\n==========================";
+    }
+
+    public void deposit(double value) {
+        this.amountUser += value;
+        System.out.println("Valor depositado com sucesso R$" + this.amountUser);
+    }
+
+    public void withdrawn(double value) {
+        if (value > this.amountUser) {
+            System.out.println("Você não pode retirar esse valor R$" + value + " Seu valor atual R$" + this.amountUser);
+        } else {
+            this.amountUser -= value;
+            System.out.println("Valor retirado com sucesso R$" + value + " Seu valor atual R$" + this.amountUser);
+        }
+    }
+
+    public void makeLoan(double value){
+        System.out.println("emprestimo R$" + value);
     }
 }
 
@@ -30,24 +53,21 @@ class Person {
     String name;
     int age;
     String work;
-    double money;
+    double salary;
 
-    public Person(String name, int age, String work, double money){
+    public Person(String name, int age, String work, double salary){
         this.name = name;
         this.age = age;
         this.work = work;
-        this.money = money;
-        System.out.println("========================");
-        System.out.println("Olá, " + this.name + " seus dados:\nIdade: " + this.age +"\nTrabalho: " + this.work +"\nDinheiro: " + this.money);
-        System.out.println("========================");
+        this.salary = salary;
     }
 }
 
 class Manager extends Person {
     boolean persmissions;
 
-    public Manager(String name, int age, String work, double money, boolean persmissions) {
-        super(name, age, work, money);
+    public Manager(String name, int age, String work, double salary, boolean persmissions) {
+        super(name, age, work, salary);
         this.persmissions = persmissions;
     }
 }
