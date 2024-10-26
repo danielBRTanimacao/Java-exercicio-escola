292646a09000ff843d1baef3bb59ab8335dad534
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-10-26 13:22:05 -0300

    abstract methods OO

--- estudo-java-yt/src/main/java/com/daniel/Poo.java
+++ estudo-java-yt/src/main/java/com/daniel/Poo.java
@@ -2,6 +2,40 @@ package com.daniel;
 
 public class Poo {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        Player p = new Player();
+        p.take();
+        p.kick();
+    }
+}
+
+abstract class Person {
+    protected String name;
+
+    public Person(String name){
+        this.name = name;
+        System.out.println("Person " + name);
+    }
+
+    public abstract void kick();
+
+    public void take(){
+        System.out.println("Pegou algo");
+    }
+}
+
+class Player extends Person {
+    public Player() {
+        super("Bico seco");
+        System.out.println("Player " + name);
+    }
+
+    @Override
+    public void take() {
+        System.out.println(name + " Pegou algo");
+    }
+
+    @Override
+    public void kick() {
+        System.out.println("Bicuda");
     }
 }
