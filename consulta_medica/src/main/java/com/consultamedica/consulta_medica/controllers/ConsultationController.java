7a31b86a785f76e7e561e79474837ad3d7ccb2ba
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-01-29 10:25:23 -0300

    refactor: resolving some bugs on service and repository

---
+++ consulta_medica/src/main/java/com/consultamedica/consulta_medica/controllers/ConsultationController.java
@@ -0,0 +1,34 @@
+package com.consultamedica.consulta_medica.controllers;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.consultamedica.consulta_medica.entities.Consultation;
+import com.consultamedica.consulta_medica.services.ConsultationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/consultas")
+public class ConsultationController {
+    @Autowired
+    private ConsultationService consultationService;
+
+    @PostMapping("/agendar")
+    public ResponseEntity<Consultation> scheduleAppointment(
+        @RequestParam Long pacientId,
+        @RequestParam Long medicId,
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime) {
+        
+        Consultation consult = consultationService.scheduleConsultation(pacientId, medicId, dateTime);
+        return ResponseEntity.ok(consult);
+    }
+    
+}
