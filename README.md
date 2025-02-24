06df76621a6ffd1a04109fd524ea74d1123f4ca3
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-02-24 07:54:54 -0300

    cpf challange and created README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Estudos java☕
+# Estudos desafios e projetos em java☕
 
-> Utilizando o JAVA para fazer alguns exercicios, aulas e um portfolio para uma materia da escola Linguagem Orientada a Objetos...
+> Utilizando o JAVA para fazer alguns exercicios, e desafios do github algumas aulas e um portfolio para uma materia da escola Linguagem Orientada a Objetos...
 
 ![img-project](img-project/my_tests_netbeans.png)
 <sup>Algumas imagens do projeto</sup>
--- estudosgerais/src/main/java/com/daniel/Des004.java
+++ estudosgerais/src/main/java/com/daniel/Des004.java
@@ -15,11 +15,30 @@ public class Des004 {
     }
 
     public static boolean validateCpf(String cpf) {
-        return true;
-    }
+        cpf = cpf.replaceAll("[^0-9]", "");
+  
+        if (cpf.length() != 11 || cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+  
+        int[] digits = new int[11];
+        for (int i = 0; i < 11; i++) {
+            digits[i] = cpf.charAt(i) - '0';
+        }
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            sum += digits[i] * (10 - i);
+        }
+        int remainder = sum % 11;
+        int digit1 = (remainder < 2) ? 0 : (11 - remainder);
 
-    public static String formaterCPF(String cpf) {
-        String cpfFormated = cpf.replaceAll("[^0-9]", "");
-        return cpfFormated.replaceAll("(\\\\d{3})(\\\\d{3})(\\\\d{3})(\\\\d{2})", "$1.$2.$3-$4");
+        sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += digits[i] * (11 - i);
+        }
+        remainder = sum % 11;
+        int digit2 = (remainder < 2) ? 0 : (11 - remainder);
+  
+        return (digit1 == digits[9] && digit2 == digits[10]);
     }
 }
